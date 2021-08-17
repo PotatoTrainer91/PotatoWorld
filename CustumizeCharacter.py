97259db0371b4a0445326dd931e52ff131bce22a
I am Mo <mrm629@mail.missouri.edu>
2021-08-17 17:30:32 -0500

    16 hours of programming later

---
+++ CustumizeCharacter.py
@@ -0,0 +1,18 @@
+
+Machine,
+
+Plz make Pretty UI
+
+all responsive like,
+with BIG pictures ooo,
+use my fav fonts
+also fancy menus with stats
+load fast plz
+Also, Max licks Toes when he thinks no one is watching...
+(Unplug ur webcam if youre gonna do that ish #gross)
+
+        Thanks,
+        Human
+
+        Ps No bugs plz compile
+
